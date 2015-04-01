c1e73377fc422f4b766f071af056299c32ca053d
Alex Hawkins <alexhawkins.me@gmail.com>
2015-04-01 11:29:14 -0700

    configure mixin file for speedy styling

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,45 @@
-const webpack = require('webpack');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
-const path = require("path");
+var webpack = require('webpack');
+var path = require("path");
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var node_modules = path.join(__dirname, 'node_modules');
 
+var config = {
 
-module.exports = {
-    devServer: {
-        contentBase: 'public'
-            // publicPath: 'public'
+    addVendor: function(name, path) {
+        this.resolve.alias[name] = path;
+        this.module.noParse.push(path);
     },
-    devtool: 'source-map',
+    context: __dirname,
+    devServer: {
+        historyApiFallback: true,
+        contentBase: 'public',
+        stats: {
+            colors: true,
+            reasons: true
+        }
+    },
+    //devtool: 'source-map',
     debug: true,
     cache: false,
 
-    context: __dirname,
-    publicPath: 'public',
+
+    entry: {
+        app: ["./src/app/UberDoku.Module.es6"],
+        vendor: ['jquery', 'underscore', 'es6-promise']
+    },
+
+    output: {
+        path: process.env.NODE_ENV === 'production' ? './dist' : './public',
+        filename: 'uberdoku.bundle.js',
+        //sourceMapFilename: 'bundle.js.map',
+        publicPath: '/public/'
+    },
 
     plugins: [
-        new webpack.optimize.CommonsChunkPlugin('common.js', 2),
-        new ExtractTextPlugin("bundle.css"),
+        new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.bundle.js', Infinity),
+        new ExtractTextPlugin("uberdoku.bundle.css", {
+            allChunks: true
+        }),
         new webpack.ProvidePlugin({
             _: "underscore",
             $: "jquery",
@@ -26,19 +48,9 @@ module.exports = {
             "root.jQuery": "jquery"
         })
     ],
-    entry: {
-        app: ["./src/app/UberDoku.Module.es6"]
-    },
-    output: {
-        path: process.env.NODE_ENV === 'production' ? './dist' : './public',
-        filename: 'bundle.js',
-        sourceMapFilename: 'bundle.js.map'
-    },
-    stats: {
-        colors: true,
-        reasons: true
-    },
+
     module: {
+        noParse: [],
         loaders: [{
                 test: /\.js$/,
                 loader: 'babel-loader',
@@ -49,7 +61,7 @@ module.exports = {
                 exclude: /node_modules/
             }, {
                 test: /\.scss$/,
-                loader: ExtractTextPlugin.extract("style-loader", "css-loader!autoprefixer-loader?browsers=last 2 version!sass-loader?includePaths[]=" + path.resolve(__dirname, "./src")),
+                loader: ExtractTextPlugin.extract("style-loader", "css-loader!autoprefixer-loader?browsers=last 2 version!sass-loader?includePaths[]=" + path.resolve(__dirname, "./src/aap")),
                 exclude: /node_modules/
             }, {
                 test: /\.html$/,
@@ -69,13 +81,17 @@ module.exports = {
         ]
     },
     resolve: {
-        root: __dirname,
+        alias: {
+            'jquery': node_modules + '/jquery/dist/jquery.min.js',
+            'underscore': node_modules + '/underscore/underscore-min.js',
+            'es6-promise': node_modules + '/es6-promise/dist/es6-promise.min.js'
+        },
         extensions: [
             '',
+            '.min.js',
             '.js',
             '.es6',
             '.es6.js',
-            '.json',
             '.webpack.js',
             '.web.js',
             '.sass',
@@ -84,6 +100,19 @@ module.exports = {
             '.html'
         ],
         modulesDirectories: ['src/app', 'node_modules', 'web_modules']
-    },
+    }
 
 };
+
+config.addVendor('jquery', path.resolve(node_modules, 'jquery/dist/jquery.min.js'));
+config.addVendor('underscore', node_modules + '/underscore/underscore-min.js');
+config.addVendor('es6-promise', node_modules + '/es6-promise/dist/es6-promise.min.js');
+
+
+if (process.env.NODE_ENV === 'production') {
+    config.plugins.push(
+        new webpack.optimize.UglifyJsPlugin(),
+        new webpack.optimize.DedupePlugin()
+    );
+}
+module.exports = config;
